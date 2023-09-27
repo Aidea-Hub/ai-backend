491fa4fe9c76382ae2e1e9845aaa93dbde1db148
Marcus Tang <txk.marcus@gmail.com>
2023-09-27 12:13:17 +0800

    Add custom llm chain

--- generateReflect/main.py
+++ generateReflect/main.py
@@ -1,37 +1,63 @@
+import logging
 import os
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
-from langchain_experimental.smart_llm import SmartLLMChain
+from llmChain import SmartLLMChain
+import functions_framework
 
 
-# Get the OpenAI API key from the environment variable
-api_key = os.environ.get("OPENAI_API_KEY")
+# Set your OpenAI API key as an environment variable (you'll set it in the Cloud Console)
+# api_key = os.environ.get("OPENAI_API_KEY")
 
-# Check if the API key is set
-if api_key is None:
-    raise ValueError("OPENAI_API_KEY environment variable is not set")
+@functions_framework.http
+def generateReflect(request):
+    os.environ[
+                "OPENAI_API_KEY"
+            ] = "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"
+    # Set CORS headers for the preflight request
+    if request.method == "OPTIONS":
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "3600",
+        }
 
-def generateReflect(params):
+        return ("", 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {"Access-Control-Allow-Origin": "*"}
+
     try:
-        # Get the input text from the dictionary
-        prompt = params["prompt"]
-        format = params["format"]
-        title = params["title"]
-        description = params["description"]
+        # Get the input data from the HTTP request
+        request_data = request.get_json()
+        prompt = request_data["prompt"]
+        format = request_data["format"]
+        title = request_data["title"]
+        description = request_data["description"]
 
         template = f"""
         {prompt}
+
+        Desired format:
         {format}
 
-        Title: {title}
-        Description: {description}
+        Idea: \"\"\"
+        {title}
+        {description}
+        \"\"\"
         """
+
+        logging.info(template)
 
         # Initialize your prompt and ChatOpenAI model
         prompt_template = PromptTemplate.from_template(template)
-        print(prompt_template)
+        logging.info(prompt_template)
+
         creative_llm = ChatOpenAI(temperature=0.9, model_name="gpt-3.5-turbo")
-        base_llm = ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo")
+        base_llm = ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo")
 
         chain = SmartLLMChain(
             ideation_llm=creative_llm,
@@ -40,25 +66,11 @@ def generateReflect(params):
             n_ideas=2,
             verbose=True
         )
-        # Run the chain
-        result = chain.run({})
-        print(result)
 
-        return {"result": result}, 200
+        # Run the chain
+        response = chain.run({})
+        return {"response": response}, 200, headers
 
     except Exception as e:
+        logging.error(str(e))
         return {"error": str(e)}, 500
-
-
-
-p = "For the following idea, suggest 10 of the most useful and innovative product capabilities. A common pitfall is failing to consider how the product serves the problem in the process. Consider your product's capabilities - does your product have the capabilities to succeed in your chosen problem space?"
-f = "Give your answers in a list, and include a conclusion at the end."
-t = "TravelBuddy: Local Experience Curation"
-d = "A platform where locals can offer personalized travel experiences or guides to tourists. Unlike generic travel services, each itinerary is tailored to individual preferences and interests."
-
-# Test the function locally
-result, status_code = generateReflect(
-    {"prompt": p, "format": f, "title": t, "description": d}
-)
-print("Status Code:", status_code)
-print("Result:", result)
